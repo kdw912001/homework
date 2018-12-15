ed91fd7fcccd7a8e16178de67e721fe8bcae0abd
kdw912001 <kdw912001@naver.com>
2018-12-16 02:39:11 +0900

    Initial commit

---
+++ day20Silsub/src/day20/silsub1/MyNote.java
@@ -0,0 +1,86 @@
+package day20.silsub1;
+import java.util.*;
+import java.io.*;
+
+
+public class MyNote {
+	private Scanner sc = new Scanner(System.in);
+	public void fileSave() {
+		System.out.print("파일에 저장할 내용을 입력하시오");
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		while(!(line = sc.nextLine()).equals("exit")) {
+			sb.append("\n"); //이걸 해야 엔터를 했을 때 입력
+			sb.append(line);
+		}
+		System.out.print("저장하시겠습니까?(y/n) : ");
+		char ch = ' ';
+		if((ch = sc.next().toUpperCase().charAt(0))=='Y') {
+			System.out.print("저장할 파일명 : ");
+			String fileName = sc.next();
+			
+			try (BufferedWriter bout = new BufferedWriter(
+						new FileWriter(fileName))){
+							//bout.write(sb.toString().getBytes());			
+						bout.write(sb.toString());
+						
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void fileOpen() {
+		System.out.print("열기할 파일명 : ");
+		String fileName = sc.next();
+		try (BufferedReader bin = new BufferedReader(new FileReader(fileName))){
+			/*while(true) {
+				if(bin.readLine()==null)
+					break;
+				System.out.println(bin.readLine());
+				
+			}*/
+			String s = null;
+			while((s=bin.readLine()) !=null) {
+				//readLine()이 없으면 null임 별짓을 다 해봤지만 이거만 됨.
+				System.out.println(s);
+			}
+			/*while(bin.readLine() != null) {
+				System.out.println(bin.read());
+			}*/
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void fileEdit() {
+		System.out.print("수정할 파일명 : ");
+		String fileName = sc.next();
+		//FileWriter fw = null;
+		StringBuilder sb = new StringBuilder();
+		try(BufferedReader br = new BufferedReader(new FileReader(fileName));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(fileName,true))) {
+			String s = null;
+			while((s=br.readLine()) != null) {
+				sb.append("\n");
+				sb.append(s);
+			}
+			System.out.print("파일에 추가할 내용을 입력하시오. " );
+			sc.nextLine();
+			while(!(s = sc.nextLine()).equals("exit")) {
+				sb.append("\n"); //한문장 씩 읽는데 \n안해주면 다 붙어서 StringBuilder에 입력
+				sb.append(s);
+			}
+			System.out.print("변경된 내용을 파일에 추가하시겠습니까?(y/n) : ");
+			char ch = ' ';
+			if((ch=sc.next().toUpperCase().charAt(0))=='Y') {
+				bw.write(sb.toString());
+				System.out.println(fileName+" 파일의 내용이 변경되었습니다.");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+
---
+++ day20Silsub/src/day20/silsub1/TestMyNote.java
@@ -0,0 +1,36 @@
+package day20.silsub1;
+
+import java.util.Scanner;
+
+public class TestMyNote {
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		menu();
+		
+
+	}
+	public static void menu() {
+		MyNote mn = new MyNote();
+		do {
+			int no;
+			System.out.print("******   MyNote  *******\r\n" + 
+					"\r\n" + 
+					"	1. 노트 새로 만들기		//MyNote의 fileSave()\r\n" + 
+					"	2. 노트 열기		//MyNote의 fileOpen()\r\n" + 
+					"	3. 노트 열어서 수정하기	//MyNote의 fileEdit()\r\n" + 
+					"	4. 끝내기		//main() 으로 리턴\r\n" + 
+					"\r\n" + 
+					"	메뉴 선택 :");
+			no=sc.nextInt();
+			switch(no) {
+			case 1: mn.fileSave(); break;
+			case 2: mn.fileOpen(); break;
+			case 3: mn.fileEdit(); break;
+			case 4: System.out.print("종료 합니다"); return;
+			default : System.out.println("잘못 입력하셨습니다"); break;
+			}
+			
+		}while(true);
+	}
+
+}
