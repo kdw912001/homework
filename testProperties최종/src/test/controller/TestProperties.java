db0058fda4daf23833e8db9dae75dc8705168548
kdw912001 <kdw912001@naver.com>
2018-12-21 21:21:35 +0900

    과제 정리

--- testProperties최종/src/test/controller/TestProperties.java
+++ testProperties최종/src/test/controller/TestProperties.java
@@ -11,19 +11,7 @@ public class TestProperties {
 		TestProperties test = new TestProperties();
 
 		Properties prop = new Properties();
-		
-		prop.setProperty("20130412", "홍길동,기획부,38000000,0.2");
-		prop.setProperty("20130413", "김길동,개발부,39000000,0.1");
-		prop.setProperty("20130414", "이길동,운영부,33000000,0.3");
-		prop.setProperty("20130415", "박길동,영업부,32000000,0.4");
-		prop.setProperty("20130416", "김동욱,개발부,30000000,0.4");
-		
-		try{
-			prop.store(new FileWriter("empData.txt"), "empData.txt");
-		}catch(IOException e) {
-			e.printStackTrace();
-		}
-		
+				
 		Employee[] ear = test.readFile(prop);
 		test.printConsole(prop);
 		test.addEmpData(prop);
@@ -35,11 +23,11 @@ public class TestProperties {
 	public void addEmpData(Properties p) {
 		p.setProperty("20130417", "황길동,개발부,40000030,0.1");
 		p.setProperty("20130418", "홍삼,개발부,38000030,0.1");
-		try{
+		/*try{
 			p.store(new FileWriter("empData.txt"), "2개 추가");
 		}catch(IOException e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 	public Employee[] readFile(Properties p) {
 		
@@ -75,9 +63,11 @@ public class TestProperties {
 	}
 	public void saveEmpXML(Employee[] er) {
 		Properties prop = new Properties();
+		int bonusSalary;
+		System.out.println(er.length);
 		for(int i=0; i<er.length;i++) {
-			er[i].setSalay((int)(er[i].getSalay()+(er[i].getSalay()*er[i].getBouusPoint())));
-			prop.setProperty(String.valueOf(er[i].getEld()), er[i].toString());
+			bonusSalary = ((int)(er[i].getSalay()+(er[i].getSalay()*er[i].getBouusPoint())));
+			prop.setProperty(String.valueOf(er[i].getEld()), er[i].toString()+" "+String.valueOf(bonusSalary));
 		}
 		
 		try {
