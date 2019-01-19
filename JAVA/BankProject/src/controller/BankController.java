2359c5ba44d9cd0cfda1010ccb661ed36dc293a3
kdw912001 <kdw912001@naver.com>
2019-01-20 02:08:18 +0900

    고객Properties수정해야함
    customerInfo,IndividualInfo

--- JAVA/BankProject/src/controller/BankController.java
+++ JAVA/BankProject/src/controller/BankController.java
@@ -4,8 +4,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.InvalidPropertiesFormatException;
+import java.util.Iterator;
 import java.util.Properties;
+import java.util.Set;
 
 import model.vo.Bank;
 
@@ -14,9 +19,12 @@ public class BankController {
 	
 	private Properties allInfo = new Properties();//고객 전체 정보
 	private Properties customerInfo = new Properties();//고객 각각 정보를 저장하기위한 변수 
+	private Properties individualInfo = new Properties();
+	//거래를 하기 위한 Properties
+	
 	int bNo;
 	
-	public BankController() {//기본생성자에서 bankAccount.xml을 load
+	public BankController(String no) {//기본생성자에서 bankAccount.xml을 load
 		System.out.println("어서오세요 KD뱅크입니다.");
 		try {
 			allInfo.loadFromXML(new FileInputStream("bankAccount.xml"));
@@ -28,36 +36,25 @@ public class BankController {
 			e.printStackTrace();
 		}//모두 Exception 으로 처리해도 되지만 나올만한 예외를 명시했음.
 		bNo = allInfo.size();//나 같은 경우 키 값을 0부터 시작했는데 이건 자기 마음대로하면 될듯 
+		
+		try {
+			individualInfo.loadFromXML(new FileInputStream(no+".xml"));
+		} catch (Exception e) {
+			System.out.println("해당 카드와 통장은 유효하지 않습니다. 다시 넣어주세요.");
+			System.exit(0);
+		}
 	}
 	
 	public void bankInsert(Bank b) {
-			//주석은 신경 쓰지말고 주석 외의 부분만 신경 쓰면 됨.
-		/*기본생성자에 loadFromXML을 안하면 조회를 하기 위해 loadFromXML을 한번 더 해야 함.
-		 *
-		 * try {
-			pp.loadFromXML(new FileInputStream("bank.xml"));
-			bNo = pp.size();
-			pp.setProperty(String.valueOf(bNo), b.toString());
-			
-		}catch(FileNotFoundException e2) {//bank.xml파일이 없을경우
-			pp.setProperty(String.valueOf(bNo), b.toString());
-		}catch(InvalidPropertiesFormatException e1) {//bak.xml내용이 비었을 경우
-			bNo = pp.size();
-			pp.setProperty(String.valueOf(bNo), b.toString());
-		}catch (IOException e) { //기본 Exception
-			e.printStackTrace();
-		}
-		
-		try {
-			pp.storeToXML(new FileOutputStream("bank.xml"), "");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}*/
-		//bNo = allInfo.size();
 		
 		
 		allInfo.setProperty(String.valueOf(bNo), b.toString());
 		customerInfo.setProperty(String.valueOf(bNo), b.toString());
+		
+		//customerInfo 각각 xml 파일 저장시
+		//customerInfo를 이뤄야 할 것
+		//key : 거래순서
+		//value : 계좌번호, 시간, 입출금액(+/-표시), 잔액
 		try {
 			allInfo.storeToXML(new FileOutputStream("bankAccount.xml"), "고객정보");
 			customerInfo.storeToXML(new FileOutputStream(bNo+".xml"), "");
@@ -73,9 +70,68 @@ public class BankController {
 		return allInfo;//전체 계좌조회를 위해 Properties를 리턴
 	}
 	
-	public void bankSearch() {}
+	public Properties bankSearch(String keyword) {
+		//상식적으로 이름을 조회하는게 맞는데 동명이인 고려해야하니 일단 키로 search
+		//직원(관리자) 입장에서 해당 계좌를 조회하는 것이기 때문에
+		//계좌번호와 이름으로 조회하면 될듯
+		Properties searchInfo = new Properties();
+		/*for(int i=0; i<allInfo.size();i++) {
+			allInfo.get
+		}
+		allInfo.getProperty(keyword);*/
+		
+		try {
+			searchInfo.loadFromXML(new FileInputStream(keyword+".xml"));
+		} catch(FileNotFoundException e2) {//keyword.xml파일이 없을경우
+			return null;
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return searchInfo;
+	}
 	
-	public void bankDeposit() {}
+	public Properties bankDeposit(String no, int p) {//String number, String name, int p) {
+		//allInfo 에서 계좌번호와 이름을 찾고 해당 키값의 xml에 입금 시킴
+		Set<String> keys = allInfo.stringPropertyNames();
+		Iterator<String> keyIter = keys.iterator();
+		//Properties depositProp = new Properties();
+		for(int i = 0; keyIter.hasNext(); i++) {
+			String key = keyIter.next();
+			String value = allInfo.getProperty(key);
+			String[] info = value.split(",");
+			String username = info[0];
+			char gender = info[1].charAt(0);
+			int age = Integer.parseInt(info[2]);
+			String bNumber = info[3];
+			int price = Integer.parseInt(info[4]);
+			Date deposit = new GregorianCalendar().getTime();
+
+		//	if(bNumber.contains(number) && username.contains(name)) {
+			if(no.equals(key)) {
+				Bank b = new Bank(username,gender,age,bNumber,price+p,deposit);
+				
+				//depositProp.setProperty(key, b.toString());
+				individualInfo.setProperty(key, b.toString());
+				//key가 중복되기 때문에 값이 추가가 안 됨.
+				//각각 개인 통장 key는 거래순서로 해야 할듯.
+				//예를 들어
+				//1. 계좌생성
+				//2. 입금  .... 등등...
+				try {
+					individualInfo.storeToXML(new FileOutputStream(key+".xml"), "");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+			}
+			
+		}
+		return individualInfo;
+		//individualInfo.getProperty(no);
+		//return individualInfo;
+	}
 	
 	public void bankWithdraw() {}
 	
--- JAVA/BankProject/src/view/Menu.java
+++ JAVA/BankProject/src/view/Menu.java
@@ -10,7 +10,12 @@ import model.vo.Bank;
 
 public class Menu {//Properties 저장
 	private Scanner sc = new Scanner(System.in);
-	BankController bc = new BankController();
+	private String no;
+	{
+		System.out.print("본인의 카드, 통장 고유번호를 입력 : ");
+		no = sc.next();
+	}
+	BankController bc = new BankController(no);
 	
 	public void mainMenu() {
 		while(true) {
@@ -55,14 +60,31 @@ public class Menu {//Properties 저장
 		
 	}
 	public void bankAllPrint() {
-		Properties p = bc.bankAllPrint();
-		p.list(System.out);
+		Properties allProp = bc.bankAllPrint();
+		allProp.list(System.out);
 	}
 	public void bankSearch() {
-		
+		System.out.print("조회할 번호 : ");
+		String keyword = sc.next();
+		Properties searchProp = bc.bankSearch(keyword);
+		if(searchProp == null) {
+			System.out.println("조회한 번호가 없습니다. 메인메뉴로 돌아갑니다.");
+		}else 
+			searchProp.list(System.out);
 	}
 	public void bankDeposit() {
-		
+		/*System.out.print("계좌번호 입력 : ");
+		String number = sc.next();
+		System.out.print("이름 : ");
+		String name = sc.next();*/
+		System.out.print("입금할 돈 : ");
+		int price = sc.nextInt();
+		//Properties depositProp = bc.bankDeposit(number,name,price);
+		Properties depositProp = bc.bankDeposit(no,price);
+		if(depositProp.isEmpty()) 
+			System.out.println("계좌번호나 이름이 잘못되었습니다. 메인메뉴로 돌아갑니다.");
+		else
+			depositProp.list(System.out);
 	}
 	public void bankWithdraw() {
 		
